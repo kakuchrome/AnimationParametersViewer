8df13ac4da53d83a922f3bff753f6227632512a9
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-03-09 19:27:52 +0900

    Initial commit

---
+++ AnimationParametersViewer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+public class PAPV : EditorWindow
+{
+    [MenuItem("APV/View")]
+    public static void OpenWindow()
+    {
+        GetWindow<PAPV>(false, "AnimationParametersViewer", true);
+    }
+    private void Update()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            Repaint();
+        }
+    }
+    bool _isFolded = true;
+    void OnGUI()
+    {
+        minSize = new Vector2(350, 400);
+        if (!EditorApplication.isPlaying)
+        {
+            EditorGUILayout.LabelField("Only works in Play mode.");
+            return;
+        }
+        VRCAvatarDescriptor[] descriptors = FindObjectsOfType<VRCAvatarDescriptor>();
+        VRCAvatarDescriptor targetDescriptor = null;
+        foreach (VRCAvatarDescriptor descriptor in descriptors)
+        {
+            if (!descriptor.gameObject.name.Contains("ShadowClone") && !descriptor.gameObject.name.Contains("MirrorReflection"))
+            {
+                targetDescriptor = descriptor;
+            }
+        }
+        if (targetDescriptor == null)
+        {
+            EditorGUILayout.LabelField("Target Avatar: Not Found");
+            return;
+        }
+        EditorGUILayout.LabelField("Target Avatar: " + targetDescriptor.gameObject.name);
+        var animator = targetDescriptor.gameObject.GetComponent<Animator>();
+        var animatorParameters = animator.parameters;
+        int[] columnsizes = { 200, 50, 100 };
+        if (animatorParameters != null)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Name", GUILayout.Width(columnsizes[0]));
+            EditorGUILayout.LabelField("Type", GUILayout.Width(columnsizes[1]));
+            EditorGUILayout.LabelField("Value", GUILayout.Width(columnsizes[2]));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginVertical("box");
+            foreach (var param in animatorParameters)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(param.name, GUILayout.Width(columnsizes[0]));
+                EditorGUILayout.LabelField(param.type.ToString(), GUILayout.Width(columnsizes[1]));
+                string valueText = "";
+                switch (param.type)
+                {
+                    case AnimatorControllerParameterType.Bool:
+                        valueText = animator.GetBool(param.name).ToString();
+                        break;
+                    case AnimatorControllerParameterType.Float:
+                        valueText = animator.GetFloat(param.name).ToString();
+                        break;
+                    case AnimatorControllerParameterType.Int:
+                        valueText = animator.GetInteger(param.name).ToString();
+                        break;
+                }
+                EditorGUILayout.LabelField(valueText, GUILayout.Width(columnsizes[2]));
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+
+
+        //折りたたみUIを表示、現在開いているかを取得
+        _isFolded = EditorGUILayout.Foldout(_isFolded, "見たいParametersがない場合");
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.wordWrap = true;
+        //開いている時はGUI追加
+        if (_isFolded)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.LabelField("このツールはAvatars3.0EmulatorでDefaultAnimatorToDebugに設定されているレイヤのParaemetersを表示します。この値は通常Baseレイヤーになっています。多くの場合、FXに変更する必要があります。この変更はツールバー/Tools/Avatars3.0Emulator/Settingsから行って下さい。", style);
+            EditorGUI.indentLevel--;
+        }
+    }
+}
